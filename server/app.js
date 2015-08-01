7748c46634ee64cdf7f1a2a1768a55a5e0912d9d
devmeyster <arthurfbi@yahoo.com>
2015-07-31 23:47:29 -0700

    Set up middleware
    - Set up routes for '/' and 'login'

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ var app = express();
 
 app.use(express.static('client'));
 
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
