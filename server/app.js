d1981a17818c539caa5bb854c06dc99d4716116a
devmeyster <arthurfbi@yahoo.com>
2015-07-31 18:20:40 -0700

    Add server middleware file

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+
 app.use(express.static('client'));
 
 var server = app.listen(3000, function () {
@@ -9,3 +10,5 @@ var server = app.listen(3000, function () {
 
   console.log('Listening at http://%s:%s', host, port);
 });
+
+require('./config/middleware.js')(app, express);
